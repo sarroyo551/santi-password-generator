29788e1de57528e355ba37fcf58e42c61543c3ca
Santi Arroyo <santi.arroyo551@gmail.com>
2023-03-30 16:36:33 -0400

    made a series of if statements to check if my prompts were valid

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,63 @@
 // Assignment code here
 function generatePassword() {
   /*ask the user for the the desired length and verify that the answer is valid*/
-  var length = prompt(`How long do you want your password to be?
-Your password must be between 8 characters and 128 characters`);
-  console.log(length)
-  /* ask the user about lowercase */
-  /*ask about uppercase*/
-  /*ask about numeric*/
-  /*ask about special characters*/
+  var possibleChars = '';
+
+  var length = Number(prompt(`How long do you want your password to be?
+Your password must be between 8 characters and 128 characters`));
+
+  // console.log(length, typeof length)
+  if (!length) {
+    alert('Must enter a number between 8 and 128')
+    return
+  }
+
+  if (length > 128 || length < 8) {
+    alert('Not a valid length')
+    return
+  } 
+
+  var lowerCase = prompt(`Does your password have lower case characters?
+  yes/no`)
+
+  if (lowerCase !== 'yes' && lowerCase !== 'no') {
+    alert('Not a valid response')
+    return
+  }
+
+  var upperCase = prompt(`Does your password have upper case characters
+  yes/no`)
+
+  if (upperCase !== 'yes' && upperCase !== 'no') {
+    alert('Not a valid response')
+    return
+  }
+
+  var numeric = prompt(`Does your password have numbers?
+  yes/no`)
+
+  if (numeric !== 'yes' && numeric !== 'no') {
+    alert('Not a valid response')
+    return
+  }
+
+  var special = prompt(`Does your password have special characters?
+  yes/no`)
+
+  if (special !== 'yes' && special !== 'no') {
+    alert('Not a valid response')
+    return
+  }
+
+  if (lowerCase == 'no' && upperCase == 'no' && numeric == 'no' && special == 'no') {
+    alert('You must choose at least one character type')
+    return
+  }
+
+  if (lowerCase == 'yes') {
+    possibleChars = possibleChars + '' //<lowercase alphabet... after all . console log 
+
+  }
 }
 
 // Get references to the #generate element
