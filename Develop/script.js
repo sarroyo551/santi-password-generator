0222735082467289a7d2627bc3ed7b1918fa5b21
Santi Arroyo <santi.arroyo551@gmail.com>
2023-03-30 15:56:29 -0400

    started the generatepassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,20 @@
 // Assignment code here
-
+function generatePassword() {
+  /*ask the user for the the desired length and verify that the answer is valid*/
+  var length = prompt(`How long do you want your password to be?
+Your password must be between 8 characters and 128 characters`);
+  console.log(length)
+  /* ask the user about lowercase */
+  /*ask about uppercase*/
+  /*ask about numeric*/
+  /*ask about special characters*/
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
